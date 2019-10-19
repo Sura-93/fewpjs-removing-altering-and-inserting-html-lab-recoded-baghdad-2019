af9706707fc024c1e0ef4bfe608fadb6acabb8fe
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-10-19 12:08:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ main.remove();
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.setAttribute("id", "victory");
-newHeader.innerHTML();
+document.querySelector('h1#victory').innerHTML= "is the champion";
